82c0e3f76c3cba0a22c95223215f30b07b56f4e9
German Natale <germannatale@gmail.com>
2022-01-17 21:55:43 -0300

    Cleaning & set favicon

--- resources/views/dashboard/base.blade.php
+++ resources/views/dashboard/base.blade.php
@@ -8,21 +8,8 @@
     <meta name="description" content="DBMS Tester - Pruebas de rendimiento para motores de Bases de Datos">
     <meta name="author" content="UTN FRLP Catedra Administración de Bases de Datos 2021 Grupo 7">
     <meta name="keyword" content="dbms,tester,db,utn,velocidad,bases de datos">
-    <title>DBMS Tester</title>
-    <link rel="apple-touch-icon" sizes="57x57" href="assets/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="assets/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="assets/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="assets/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="assets/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="assets/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="assets/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="assets/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="assets/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="assets/favicon/manifest.json">
+    <title>DBMS Tester</title>    
+    <link rel="icon" type="image/png" href="assets/favicon/favicon.png">    
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="assets/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
